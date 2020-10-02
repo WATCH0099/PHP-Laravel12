567c8919df37bb0c84237a347d5cbc22117179cf
”Kanji-Kurata” <kurayan.first@gmail.com>
2020-10-02 11:41:15 +0000

    LARAVEL13

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::group(['prefix' => 'admin'], function() {
+    Route::post('profile/create', 'Admin\ProfileController@create')->middleware('auth');
     Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
-    Route::get('profile/edit', 'Admin\ProfileController@add')->middleware('auth');
+    Route::post('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
+    Route::get('profile/edit', 'Admin\ProfileController@update')->middleware('auth');
+    
+    
+    
     Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
+    Route::post('news/create', 'Admin\NewsController@create')->middleware('auth'); # 追記
 });
 
 Auth::routes();
