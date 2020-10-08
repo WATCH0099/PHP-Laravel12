44c9e325b29e8fd95325f87678100e82f41a1dcb
”Kanji-Kurata” <kurayan.first@gmail.com>
2020-10-08 21:12:16 +0000

    LARAVEL16

--- resources/views/layouts/profile.blade.php
+++ resources/views/layouts/profile.blade.php
@@ -1,57 +1,39 @@
-<!DOCTYPE html>
+<!DOCTIME html>
 <html lang="{{ app()->getLocale() }}">
     <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-compatible" content="IE-edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <!-- CSRF Token -->
-         {{-- 後の章で説明します --}}
+        
         <meta name="csrf-token" content="{{ csrf_token() }}">
-
-        {{-- 各ページごとにtitleタグを入れるために@yieldで空けておきます。 --}}
+        
         <title>@yield('title')</title>
-
-        <!-- Scripts -->
-         {{-- Laravel標準で用意されているJavascriptを読み込みます --}}
-        <script src="{{ secure_asset('js/app.js') }}" defer></script>
-
-        <!-- Fonts -->
-        <link rel="dns-prefetch" href="https://fonts.gstatic.com">
+        
+        <script src="{{ secure_asset('js/app.js') }}" defar></script>
+        
+        <link rel="dns-prefetch" href="http://fonts.gstatic.com">
         <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-        {{-- Laravel標準で用意されているCSSを読み込みます --}}
+        
         <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
-        {{-- 下記のコードが書き換えられている --}}
         <link href="{{ secure_asset('css/profile.css') }}" rel="stylesheet">
+        
     </head>
     <body>
         <div id="app">
-            {{-- 画面上部に表示するナビゲーションバーです。 --}}
             <nav class="navbar navbar-expand-md navbar-dark navbar-laravel">
                 <div class="container">
                     <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
+                        {{ config('app.name', 'laravel') }}
                     </a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <button class="navbar-toggler" type="button" date-toggle="collapse" date-target="#navbarSupportedcontent" aria-controls="navbarSupportedcontent" aria-expanted="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
-
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <!-- Left Side Of Navbar -->
+                    
+                    <div class="collapse navbar-collapse" id="navbarSupportedcontent">
                         <ul class="navbar-nav mr-auto">
-
-                        </ul>
-
-                        <!-- Right Side Of Navbar -->
-                        <ul class="navbar-nav ml-auto">
-                             {{-- 以下を追記 --}}
-                        <!-- Authentication Links -->
-                        {{-- ログインしていなかったらログイン画面へのリンクを表示 --}}
+                            <!-- Authentication Links -->
                         @guest
                             <li><a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a></li>
-                        {{-- ログインしていたらユーザー名とログアウトボタンを表示 --}}
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
@@ -71,15 +53,15 @@
                                 </div>
                             </li>
                             @endguest
-                            {{-- 以上までを追記 --}}
+                        </ul>
+                        
+                        <ul class="navbar-nav ml-auto">
+                            
                         </ul>
                     </div>
                 </div>
             </nav>
-            {{-- ここまでナビゲーションバー --}}
-
             <main class="py-4">
-                {{-- コンテンツをここに入れるため、@yieldで空けておきます。 --}}
                 @yield('content')
             </main>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -14,16 +14,22 @@
 Route::get('/', function () {
     return view('welcome');
 });
-Route::group(['prefix' => 'admin'], function() {
-    Route::post('profile/create', 'Admin\ProfileController@create')->middleware('auth');
-    Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
-    Route::post('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
-    Route::get('profile/edit', 'Admin\ProfileController@update')->middleware('auth');
+Route::group(["prefix"=>"admin", "middlware"=>"auth"], function() {
+    Route::post('profile/create', 'Admin\ProfileController@create');
+    Route::get('profile/create', 'Admin\ProfileController@add');
+    Route::post('profile/edit', 'Admin\ProfileController@edit');
+    Route::get('profile/edit', 'Admin\ProfileController@update');
     
+    Route::post('profile/edit', 'Admin\ProfileController@update');
+    Route::get('profile/delete', 'Admin\ProfileController@delete');
     
+    Route::get('news/create', 'Admin\NewsController@add');
+    Route::post('news/create', 'Admin\NewsController@create');
+    Route::get('news', 'Admin\NewsController@index');
+    Route::get('news/edit', 'Admin\NewsController@edit');
+    Route::post('news/edit', 'Admin\NewsController@update');
     
-    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
-    Route::post('news/create', 'Admin\NewsController@create')->middleware('auth'); # 追記
+    Route::get('news/delete', 'Admin\NewsController@delete');
 });
 
 Auth::routes();
